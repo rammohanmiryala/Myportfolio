a979a115c1a7a4ab581f12c5ee64d0f570a9bac2
rammohanmiryala <rammohanmiryala@hotmail.com>
2021-11-03 14:38:25 +1030

    added js file but errors

---
+++ assets/js/index.js
@@ -0,0 +1,18 @@
+var nightmodeEl = $('.nightmode')
+document.getElementById("switch").addEventListener("change", function () {
+    if (document.getElementById("switch").checked == true) {
+
+        nightmodeEl.setAttribute("style", "background-color: #FFD4D4;");
+        nightmodeEl.setAttribute("style", "color: white;");
+
+
+
+        //   document.documentElement.setAttribute("style", "background-color: #FFD4D4;");
+        //   document.getElementsByTagName(".nightmode")[1].setAttribute("style", "color: white;");
+
+    } else {
+        nightmodeEl.setAttribute("style", "background-color: #222222;");
+        nightmodeEl.setAttribute("style", "color: #FFD4D4;");
+
+    }
+});
