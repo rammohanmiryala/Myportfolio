687decb3e66fd0892dfd1e99bcfe7a082ba1cde2
rammohanmiryala <rammohanmiryala@hotmail.com>
2021-11-03 15:45:57 +1030

    fixed js file error

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,18 +1,18 @@
-var nightmodeEl = document.getElementById('.nightmode')
-document.getElementById("switch").addEventListener("change", function () {
-    if (document.getElementById("switch").checked == true) {
+var nightmodeEl = document.querySelector('.nightmode')
+var pagecolorel =document.querySelector('.pagecolor')
 
-        nightmodeEl.setAttribute("style", "background-color: #FFD4D4;");
-        nightmodeEl.setAttribute("style", "color: white;");
+document.querySelector("#switch").addEventListener("change", function () {
+    if (document.querySelector("#switch").checked == true) {
 
-
+        nightmodeEl.setAttribute("style", "background-color: #DFDCDC; color: #473F45;");
+        pagecolorel.setAttribute("style", "color: #473F45;");
 
         //   document.documentElement.setAttribute("style", "background-color: #FFD4D4;");
         //   document.getElementsByTagName(".nightmode")[1].setAttribute("style", "color: white;");
 
     } else {
-        nightmodeEl.setAttribute("style", "background-color: #222222;");
-        nightmodeEl.setAttribute("style", "color: #FFD4D4;");
+        nightmodeEl.setAttribute("style", "background-color: #222222; color: #FFD4D4;");
+        pagecolorel.setAttribute("style", "color: #FFD4D4;");
 
     }
 });
