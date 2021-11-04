bc6ee42b2ca8f2c259dad8aeae64ba14f3e49902
rammohanmiryala <rammohanmiryala@hotmail.com>
2021-11-04 16:16:34 +1030

    added final changes with responsive

--- README.md
+++ README.md
@@ -1,10 +1,42 @@
-# Myportfolio
+# My Portfolio
 
 ## About The Project
+
 This project is to show my worked projects till date
 
-## acceptance
+## Technologies used
+
+- html
+- css
+- java script
+
+## Acceptance
+
 - this is single page application
 - images increase by image by 1.5 scale
 - the application is responsive for all devices
 - navigation is linked to corresponding section
+
+# Prototype
+
+The following shows how the application is designed
+- https://www.figma.com/proto/KQII2xfHxFLaZA7gGBMvVe/Untitled?page-id=0%3A1&node-id=1%3A3&viewport=338%2C48%2C0.6&scaling=contain&starting-point-node-id=1%3A3&show-proto-sidebar=1
+
+
+# final work
+
+The following appearance of the site:
+
+![user can check my complete profile.](./assets/images/portfo.png)
+
+## Development process
+
+- HTML and CSS pages are created
+- added styling
+- added comments to HTML and CSS
+
+## The following appearance of the site:
+
+# URL of the website
+
+https://rammohanmiryala.github.io/Myportfolio/
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,29 +1,26 @@
 var nightmodeEl = document.querySelector('.nightmode')
-var pages1el =document.querySelector('.pages1')
-var pages2el =document.querySelector('.pages2')
-var pages3el =document.querySelector('.pages3')
-var pages4el =document.querySelector('.pages4')
-var textxcolorpel =document.querySelector('.textxcolorp')
-var facebookel =document.querySelector('.facebook')
-var linkedinel =document.querySelector('.linkedin')
-var twitterel =document.querySelector('.twitter')
+var pages1el = document.querySelector('.pages1')
+var pages2el = document.querySelector('.pages2')
+var pages3el = document.querySelector('.pages3')
+var pages4el = document.querySelector('.pages4')
+var textxcolorpel = document.querySelector('.textxcolorp')
+var facebookel = document.querySelector('.facebook')
+var linkedinel = document.querySelector('.linkedin')
+var twitterel = document.querySelector('.twitter')
+var switchel = document.querySelector("#switch")
+
+localStorage.setItem("switch1", switchel.value);
 
 
-
-
-
-document.querySelector("#switch").addEventListener("change", function () {
-    if (document.querySelector("#switch").checked == true) {
+function checkedvalue() {
+    if (switchel.checked == true) {
 
         nightmodeEl.setAttribute("style", "background-color: #DFDCDC; color: #473F45;");
         pages1el.setAttribute("style", "color: #473F45; font-weight: bold;");
         pages2el.setAttribute("style", "color: #473F45;font-weight: bold;");
         pages3el.setAttribute("style", "color: #473F45;font-weight: bold;");
         pages4el.setAttribute("style", "color: #473F45;font-weight: bold;");
-        textxcolorpel.setAttribute("style", "color: #222222;");
-        facebookel.setAttribute("style", "color: #222222;");
-        linkedinel.setAttribute("style", "color: #222222;");
-        twitterel.setAttribute("style", "color: #222222;");
+
         //   document.documentElement.setAttribute("style", "background-color: #FFD4D4;");
         //   document.getElementsByTagName(".nightmode")[1].setAttribute("style", "color: white;");
 
@@ -34,10 +31,12 @@ document.querySelector("#switch").addEventListener("change", function () {
         pages3el.setAttribute("style", "color: #FFD4D4;");
         pages4el.setAttribute("style", "color: #FFD4D4;");
         textxcolorpel.setAttribute("style", "color: #ececec;");
-        facebookel.setAttribute("style", "color: #FFD4D4;");
-        linkedinel.setAttribute("style", "color: #FFD4D4;");
-        twitterel.setAttribute("style", "color: #FFD4D4;");
+
 
 
     }
-});
+
+}
+
+
+switchel.addEventListener("change", checkedvalue)
